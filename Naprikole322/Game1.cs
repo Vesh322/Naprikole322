2beda40c85a9cbf0152ba7256650dc87448a453f
pahsa <pahsa@EBANAYAKOROBKA>
2023-06-08 18:58:17 +0500

    Добавьте файлы проекта.

---
+++ Naprikole322/Game1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using Tutorial020.Sprites;
+using Tutorial020.States;
+
+namespace Tutorial020
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SoundEffect soundEffect;
+
+        public static Random Random;
+
+        public static int ScreenWidth = 1280;
+        public static int ScreenHeight = 720;
+
+        private State _currentState;
+        private State _nextState;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            Random = new Random();
+
+            graphics.PreferredBackBufferWidth = ScreenWidth;
+            graphics.PreferredBackBufferHeight = ScreenHeight;
+            graphics.ApplyChanges();
+
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            soundEffect = Content.Load<SoundEffect>("shot");
+
+            _currentState = new MenuState(this, Content);
+            _currentState.LoadContent();
+            _nextState = null;
+        }
+
+        protected override void UnloadContent()
+        {
+        
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (_nextState != null)
+            {
+                _currentState = _nextState;
+                _currentState.LoadContent();
+
+                _nextState = null;
+            }
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                    soundEffect.Play();
+                SoundEffect.MasterVolume = 0.2f;
+            }
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(55, 55, 55));
+
+            _currentState.Draw(gameTime, spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
